88ccde138a3d4e6ab2e25e20170066db7c0a59fc
azimabdurasulov <azimjon20050604@gmail.com>
2022-10-24 14:31:59 +0500

    While02.py

--- While02.py
+++ While02.py
@@ -6,4 +6,16 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i = 0
+    counter = 0 
+
+    while i < len(s):
+
+        if s[i].isalpha():
+            counter += 1
+
+        i += 1
+
+    return counter
+
+print(main("Python 34235"))
